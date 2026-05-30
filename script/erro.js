2905f5d4bd65fb89e48133052f665f5a11cd61a4
Isabelly Carvalho Piccinato <isabellpiccinato@gmail.com>
2026-05-30 20:27:23 -0300

    refactor: remove inline session validation script from Login.html and enhance error handling in erro.js

--- script/erro.js
+++ script/erro.js
@@ -1,6 +1,15 @@
 
 let user = JSON.parse(sessionStorage.getItem('user'))
 
+// validação de acesso direto
 if(user === null){
+    window.location.href = ''
+}
+
+if(user.email !== 'eve@gmail.com' || user.password !== '123456'){
     window.location.href = './erro.html'
 }
+
+let ps = document.querySelectorAll('p')
+ps[0].textContent = `EMAIL: ${user.email}`
+ps[1].textContent = `SENHA: ${user.password}`
