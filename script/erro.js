fc7e49e0568a4aace3dcc522ac85f7e892e091aa
Isabelly Carvalho Piccinato <isabellpiccinato@gmail.com>
2026-05-30 21:04:49 -0300

    refactor: remove unnecessary user validation checks in erro.js

--- script/erro.js
+++ script/erro.js
@@ -5,11 +5,3 @@ let user = JSON.parse(sessionStorage.getItem('user'))
 if(user === null){
     window.location.href = './erro.html'
 }
-
-if(user.email !== 'eve@gmail.com' || user.password !== '123456'){
-    window.location.href = './erro.html'
-}
-
-let ps = document.querySelectorAll('p')
-ps[0].textContent = `EMAIL: ${user.email}`
-ps[1].textContent = `SENHA: ${user.password}`
